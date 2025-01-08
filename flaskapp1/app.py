225f8fdb82b910fd0a649944fc41aecc0ce5c4c1
Acstrayer <nskkid@gmail.com>
2025-01-08 04:46:21 +0000

    Created web app based on Assignment1 requirements

---
+++ flaskapp1/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd6ad52c24926b2c98f099c76467bfbcc7c5eef8659916b6'
+app.static_folder = 'static'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        content = request.form['content']
+        return '{}'.format(content)
+    return render_template('index.html')
